57942576f2a781b6707a75ded50899e665f089fd
Ashley Bartlett <ashley.bartlett@gmail.com>
2026-05-11 11:54:03 +1000

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} heights
+     * @return {number}
+     */
+    maxArea(heights) {
+        let j = 0,
+            k = heights.length - 1;
+
+        let largest = 0;
+
+        while(j < k) {
+            const vol = Math.min(heights[j], heights[k]) * (k - j);
+            
+            largest = Math.max(largest, vol)
+            if(heights[j] < heights[k]) {
+                j++
+            } else {
+                k--
+            }
+        }
+        return largest
+    }
+}
