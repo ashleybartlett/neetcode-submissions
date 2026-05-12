b41b7b74b6be279ad49519d99e5111ffb1e2efdc
Ashley Bartlett <ashley.bartlett@gmail.com>
2026-05-12 14:27:49 +1000

    Add: longest-substring-without-duplicates - submission-4

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-4.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s) {
+        if (!s.length) {
+            return 0;
+        }
+        let maxS = 1;
+
+        let l = 0,
+            r = 1;
+
+        // let seen = new Set([s[l]]);
+        let seen = new Map([[s[l], l]]);
+
+        while (r < s.length) {
+            if (seen.has(s[r])) {
+                // cleanup previously seen unique letters
+                // console.log(`Cleanup [${l}, ${r}]: ${s[r]}`)
+                // console.log("Seen", seen)
+                while (l <= seen.get(s[r])) {
+                    seen.delete(s[l]);
+                    l++;
+                }
+                // console.log("Seen after", seen)
+                // l=seen.get(s[r]); // move to right of duplicate
+                // seen = new Set([s[r]]);
+            }
+
+            seen.set(s[r], r);
+
+            r++;
+            maxS = Math.max(maxS, r - l);
+        }
+        return maxS;
+    }
+}
